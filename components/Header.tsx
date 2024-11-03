cb05bed0fc10470634abecacddf6665ae1110f55
Yousuftayyaba <Yousuftayyaba2@gmail.com>
2024-11-03 20:20:02 +0500

    figma template practice

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="h-[88px] bg-gray-300">
+      {/* <h1>This is header</h1> */}
+    </div>
+  )
+}
+
+export default Header
